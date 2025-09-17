d12f78ae130292727dd2a14965c94ad3894d880f
apostoloskalog <apostolos.kalog@gmail.com>
2025-09-17 20:01:36 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -10,12 +10,10 @@ export default function Home() {
       if (!process.env.NEXT_PUBLIC_SUPABASE_URL || !process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY) {
         throw new Error("Missing Supabase environment variables");
       }
-
       const client = createClient(
         process.env.NEXT_PUBLIC_SUPABASE_URL,
         process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
       );
-
       setSupabase(client);
     } catch (err) {
       setError(err.message);
