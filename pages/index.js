75b7c891f17c31a678d2fd838f6ef1e50b92e38c
apostoloskalog <apostolos.kalog@gmail.com>
2025-09-17 15:43:51 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -58,3 +58,6 @@ export default function Home() {
     </main>
   );
 }
+console.log("SUPABASE_URL:", process.env.NEXT_PUBLIC_SUPABASE_URL);
+console.log("SUPABASE_KEY:", process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY);
+
